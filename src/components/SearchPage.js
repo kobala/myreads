86883e5ee356efc890bb1949ae97fba917d11dc5
Irakli Kobalia <qobalia92@gmail.com>
2017-07-29 18:38:08 +0400

    fix: Remove no used imports

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import * as BooksAPI from '../utils/BooksAPI'
 import Bookshelf from './Bookshelf'
 import ProgressBar from './common/ProgressBar'
